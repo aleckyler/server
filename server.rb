b18e60a17334df7edb9d4d93e13f6404a357e57d
Alec Greenberg <aleckyler@comcast.com>
2015-07-15 11:36:10 -0400

    first Heroku commit

---
+++ server.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require '../stock_ticker.rb'
+require 'rubygems'
+
+get '/' do
+  File.read(File.join('../wyncode_folder', 'stock_ticker.rb'))
+end
+
+get '/' do
+  "Hello world"
+  stock_ticker(aapl)
+end
+
+get "/sinatra" do
+"Hello Sinatra"
+end

76d7040e8855edcd3b118c5afde9ca336b73f9cd
Alec Greenberg <aleckyler@comcast.com>
2015-07-15 11:58:43 -0400

    next

--- server.rb
+++ server.rb
@@ -1,16 +1,16 @@
 require 'sinatra'
 # require '../stock_ticker.rb'
-require 'rubygems'
-
+# require 'rubygems'
+#
+# # get '/' do
+# #   File.read(File.join('../wyncode_folder', 'stock_ticker.rb'))
+# # end
+#
 # get '/' do
-#   File.read(File.join('../wyncode_folder', 'stock_ticker.rb'))
+#   "Hello world"
+#   # stock_ticker(aapl)
 # end
-
-get '/' do
-  "Hello world"
-  # stock_ticker(aapl)
-end
-
-get "/sinatra" do
-"Hello Sinatra"
-end
+#
+# get "/sinatra" do
+# "Hello Sinatra"
+# end

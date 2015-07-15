52920a45d686e1091bf8b7da76f6ecad23fb4490
Alec Greenberg <aleckyler@comcast.com>
2015-07-15 13:38:56 -0400

    updated server.rb file for hw

--- server.rb
+++ server.rb
@@ -1,20 +1,17 @@
 require 'sinatra'
-# require '../stock_ticker.rb'
-# require 'rubygems'
-#
-# # get '/' do
-# #   File.read(File.join('../wyncode_folder', 'stock_ticker.rb'))
-# # end
-#
-# get '/' do
-#   "Hello world"
-#   # stock_ticker(aapl)
-# end
-#
-# get "/sinatra" do
-# "Hello Sinatra"
-# end
+require 'httparty'
+require 'nokogiri'
 
-get '/ hello.txt' do
+get '/' do
+  ticker = "aapl"
+  url = "http://finance.yahoo.com/q?s=#{ticker}"
+  response = HTTParty.get url
+  dom = Nokogiri::HTML(response.body)
+  ticker_span = dom.xpath("//span[@id='yfs_l84_#{ticker}']").first.content
+  prev_close_span = dom.xpath("//td[@class='yfnc_tabledata1']").first.content
+  "#{ticker.upcase} stock is currently trading at $#{ticker_span}. It's previous close price was #{prev_close_span}"
+end
 
+post '/' do
+  "Here's a post"
 end
